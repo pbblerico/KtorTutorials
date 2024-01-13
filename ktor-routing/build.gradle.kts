5a6f898d55b90c5107bd8ac2d4bcb69874ff0b25
Karina <kmirova731@gmail.com>
2024-01-13 22:57:15 +0600

    Gradle change add serialization plugin. Manual testing check

--- ktor-routing/build.gradle.kts
+++ ktor-routing/build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 group = "com.example"
